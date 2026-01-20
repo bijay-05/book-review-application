b0328001489d6ed9a55f4a0d5b273c0887a6e1c1
bijay-05 <hawkeyesniper69@gmail.com>
2026-01-20 23:15:17 +0545

    add components

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import FooterColumn from './FooterColumn'
+
+export default function Footer() {
+  return (
+    <div className="px-2 py-12 flex gap-4 justify-around bg-slate-100">
+      <FooterColumn />
+    </div>
+  )
+}
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,87 @@
+import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser } from "@fortawesome/free-regular-svg-icons";
+import {
+  faHouse,
+  faStore,
+  faMagnifyingGlass,
+} from "@fortawesome/free-solid-svg-icons";
+import { Link } from "react-router-dom";
+
+export default function Navbar() {
+  const [userProfileFlag, setUserProfileFlag] = useState(false);
+  const cartProducts = [];
+
+  const changeUserProfileFlag = () => {
+    setUserProfileFlag(!userProfileFlag);
+  };
+
+  return (
+    <div className="px-2 py-2 bg-blue-100 sticky top-0 z-10">
+      <div className="flex flex-col md:flex-row gap-4 justify-between">
+        <div className="px-2 py-1 w-fit rounded-lg bg-blue-300 cursor-pointer border-2 border-blue-200">
+          <Link to="/">
+            <img
+              className="w-[80px] h-[80px]"
+              src="/static/blogo.png"
+              alt="Logo"
+            />
+          </Link>
+        </div>
+        {/*  */}
+        <div className="flex flex-col md:flex-row gap-2 items-center">
+          <div className="w-full md:w-fit text-center bg-blue-200 rounded-md">
+            <Link
+              to="/"
+              className="font-bold block md:inline-block py-1 px-4 hover:bg-orange-300 hover:text-white hover:rounded-lg"
+            >
+              <FontAwesomeIcon icon={faHouse} />1
+              <span className="px-2">Home</span>
+            </Link>
+          </div>
+          <div className="w-full md:w-fit text-center bg-blue-200 rounded-md">
+            <Link
+              to="/search?s=&category=all"
+              className="font-bold block md:inline-block py-1 px-4 hover:bg-orange-300  hover:text-white hover:rounded-lg"
+            >
+              <FontAwesomeIcon icon={faMagnifyingGlass} />
+              <span className="px-2">Search</span>
+            </Link>
+          </div>
+          <div className="w-full md:w-fit text-center bg-blue-200 rounded-md">
+            <Link
+              to="/mystore"
+              className="font-bold block md:inline-block py-1 px-4 hover:bg-orange-300 hover:text-white hover:rounded-lg"
+            >
+              <FontAwesomeIcon icon={faStore} />
+              <span className="px-2">My Store ({cartProducts.length})</span>
+            </Link>
+          </div>
+        </div>
+        {/*  */}
+        <div className="flex items-center">
+          <div
+            className="relative px-4 py-2 border-2 border-blue-200 bg-blue-200 cursor-pointer rounded-lg ml-auto"
+            onClick={changeUserProfileFlag}
+          >
+            <FontAwesomeIcon icon={faUser} />
+            <span className="px-2">Hello, John</span>
+            {userProfileFlag && (
+              <div className="py-2 absolute top-full -right-px w-[150px] shadow-lg bg-white rounded-lg">
+                <span className="block w-full px-4 py-1 hover:bg-green-600 hover:border-green-300 hover:text-white">
+                  Profile
+                </span>
+                <span className="block w-full px-4 py-1 hover:bg-green-600 hover:text-white">
+                  Settings
+                </span>
+                <span className="block w-full px-4 py-1 hover:bg-white hover:text-red-500 font-semibold">
+                  Log Out
+                </span>
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
