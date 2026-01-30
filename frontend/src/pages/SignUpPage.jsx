e39850635f2acd31823b88e8b2c882c3d351761d
bijay-05 <hawkeyesniper69@gmail.com>
2026-01-30 22:57:02 +0545

    add login and signup pages

---
+++ frontend/src/pages/SignUpPage.jsx
@@ -0,0 +1,84 @@
+import { useState, useContext } from "react";
+import { useValidPassword, useValidUsername } from "../hooks/useAuthHooks";
+import { AuthContext } from "../contexts/authContext";
+import { useNavigate } from "react-router-dom";
+
+export default function SignUp() {
+  const { username, setUsername, usernameIsValid } = useValidUsername("");
+  const { password, setPassword, passwordIsValid } = useValidPassword("");
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
+
+  const isValid =
+    !usernameIsValid ||
+    username.length === 0 ||
+    !passwordIsValid ||
+    password.length === 0;
+
+  const authContext = useContext(AuthContext);
+
+  const signUpClicked = async () => {
+    try {
+      await authContext.signUpWithEmail(username, password);
+      navigate("/login");
+    } catch (err) {
+      if (err.code === "UserNotConfirmedException") {
+        console.log("Error: in  UserNotConfirmedException");
+      } else {
+        setError(err.message);
+      }
+    }
+  };
+
+  return (
+    <>
+      <div className="w-fit mx-auto">
+        <div className="p-8 m-8 rounded-md shadow-md  bg-violet-300">
+          <form onSubmit={signUpClicked}>
+            <div className="mb-4">
+              <label htmlFor="email" className="block text-sm font-bold mb-2">
+                Email:
+              </label>
+              <input
+                type="text"
+                id="email"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                className="w-full border border-gray-300 rounded-md px-3 py-2"
+              />
+            </div>
+            <div className="mb-4">
+              <label
+                htmlFor="password"
+                className="block text-sm font-bold mb-2"
+              >
+                Password:
+              </label>
+              <input
+                type="password"
+                id="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                className="w-full border border-gray-300 rounded-md px-3 py-2"
+              />
+            </div>
+            <div className="flex justify-center gap-4 w-[20]">
+              <button
+                onClick={() => navigate("/")}
+                className="border-2 px-4 py-2 rounded-md bg-red-600 text-white text-xl"
+              >
+                Cancel
+              </button>
+              <button
+                disabled={isValid}
+                className="border-2 px-4 py-2 rounded-md bg-blue-600 text-white text-xl hover:bg-green-600"
+              >
+                Sign Up
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+}
