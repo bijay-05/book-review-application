3a46d3d16a17e1fc12cdbde6ee1d5fb9e4970e37
bijay-05 <hawkeyesniper69@gmail.com>
2026-02-04 00:17:36 +0545

    Revert "add response module for custom response"
    This reverts commit 9050df3606cc94db90b174f7e2e891769b6aac6f.

--- backend/src/common/response/decorators/responseMessage.decorator.ts
+++
@@ -1,5 +0,0 @@
-import { SetMetadata, applyDecorators } from '@nestjs/common';
-
-export function ResponseMessage(message: string): MethodDecorator {
-  return applyDecorators(SetMetadata('responseMessage', message));
-}
