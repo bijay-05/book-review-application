d2f39e4b34f7bf61ef8b6f3015d534920526a864
bijay-05 <hawkeyesniper69@gmail.com>
2026-02-04 00:27:24 +0545

    add validation pipe and enable CORS

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,21 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { ValidationPipe } from "@nestjs/common";
+import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: "*",
+    methods: ["GET", "PUT", "POST", "PATCH", "DELETE"],
+  });
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+    }),
+  );
+
   await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
