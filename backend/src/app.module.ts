b22bae3b07c4783caec84ceb50d14d27070fd98d
bijay-05 <hawkeyesniper69@gmail.com>
2026-02-05 11:27:18 +0545

    resolve conflicts

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,20 @@
-import { Module } from '@nestjs/common';
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
 import { UserModule } from "./modules/user/user.module";
+import { BookModule } from "./modules/book/book.module";
+import { ReviewModule } from "./modules/review/review.module";
+import { AuthenticationModule } from "./modules/auth/auth.module";
+import { CommonModule } from "./common/common.module";
 
 @Module({
-  imports: [UserModule],
-  controllers: [],
+  imports: [
+    UserModule,
+    BookModule,
+    ReviewModule,
+    AuthenticationModule,
+    CommonModule,
+  ],
+  controllers: [AppController],
   providers: [],
 })
 export class AppModule {}
--- backend/src/common/common.module.ts
+++ backend/src/common/common.module.ts
@@ -1,4 +1,19 @@
 import { Module } from "@nestjs/common";
+import { ResponseModule } from "./response/response.module";
+import { PrismaModule } from "./prisma/prisma";
+import { ErrorModule } from "./error/error.module";
+import { LoggerModule } from "./logger/logger.module";
+import { DebuggerModule } from "./debugger/debugger.module";
+import { AuthModule } from "./auth/auth.module";
 
-@Module({})
+@Module({
+  imports: [
+    AuthModule,
+    ErrorModule,
+    LoggerModule,
+    DebuggerModule,
+    PrismaModule,
+    ResponseModule,
+  ],
+})
 export class CommonModule {}
