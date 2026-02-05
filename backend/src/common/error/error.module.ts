955482a6ec8fb9ec40aefa49dc1c2224c34a2469
bijay-05 <hawkeyesniper69@gmail.com>
2026-02-05 11:38:53 +0545

    add error filter

---
+++ backend/src/common/error/error.module.ts
@@ -0,0 +1,14 @@
+import { Module } from "@nestjs/common";
+import { APP_FILTER } from "@nestjs/core";
+import { ErrorFilter } from "./filters/error.filter";
+
+@Module({
+  imports: [],
+  providers: [
+    {
+      provide: APP_FILTER,
+      useClass: ErrorFilter,
+    },
+  ],
+})
+export class ErrorModule {}
