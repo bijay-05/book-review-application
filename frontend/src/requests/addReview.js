29def49c69b6658bf415fb2bff17fce0da4c056e
bijay-05 <hawkeyesniper69@gmail.com>
2026-02-10 15:11:20 +0545

    add review create API

---
+++ frontend/src/requests/addReview.js
@@ -0,0 +1,19 @@
+import { api } from "./axiosInstance";
+
+const accessToken = window.localStorage.getItem("accessToken");
+
+export const addNewReview = async ({ value, bookId }) => {
+  const response = await api.post(
+    "/review",
+    {
+      value: value,
+      bookId: Number(bookId),
+    },
+    {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    },
+  );
+  console.log("Response of Review creation: ", response.data);
+};
