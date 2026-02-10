bff2888b21f98d433cb9c6053c714335506c7246
bijay-05 <hawkeyesniper69@gmail.com>
2026-02-10 17:50:20 +0545

    add endpoint to check auth session status

--- backend/src/modules/auth/auth.controller.ts
+++ backend/src/modules/auth/auth.controller.ts
@@ -1,7 +1,13 @@
-import { Body, Controller, Post } from "@nestjs/common";
+import { Body, Controller, Post, Get } from "@nestjs/common";
 import { AuthService } from "./auth.service";
 import { UserLoginDto } from "./dtos/login.dto";
 import { ResponseMessage } from "src/common/response/decorators/responseMessage.decorator";
+import {
+  GetUser,
+  UserProtected,
+} from "src/common/auth/decorators/auth.decorator";
+import { IAuthUser } from "src/common/auth/interfaces/jwt.interface";
+import { IResponse } from "src/common/response/interfaces/response.interface";
 
 @Controller("auth")
 export class AuthController {
@@ -13,5 +19,24 @@ export class AuthController {
     const data = await this.authService.login(body);
 
     return { data };
+  }
+
+  @Get("session")
+  @UserProtected()
+  @ResponseMessage("Session retrieved successfully")
+  async getSession(
+    @GetUser() authUser: IAuthUser,
+  ): Promise<IResponse<boolean>> {
+    const userId = authUser.id;
+
+    if (userId) {
+      return {
+        data: true,
+      };
+    } else {
+      return {
+        data: false,
+      };
+    }
   }
 }
