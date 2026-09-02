359403709f1c1db0db889e6b4c601a04b6d59f13
bijay-05 <hawkeyesniper69@gmail.com>
2026-09-02 13:55:27 +0545

    update

--- backend/src/common/configs/index.ts
+++ backend/src/common/configs/index.ts
@@ -1,5 +1,6 @@
 import AppConfig from "./app.config";
 import AuthConfig from "./auth.config";
 import DataBaseConfig from "./database.config";
+import HelperConfig from "./misc.config";
 
-export default [DataBaseConfig, AuthConfig, AppConfig];
+export default [DataBaseConfig, AuthConfig, AppConfig, HelperConfig];
--- backend/src/common/configs/misc.config.ts
+++ backend/src/common/configs/misc.config.ts
@@ -10,7 +10,7 @@ export default registerAs(
     redis: {
       host: process.env.REDIS_HOST,
       port: process.env.REDIS_PORT,
-      password: process.env.REDIS_PASSWORD,
+      // password: process.env.REDIS_PASSWORD,
     },
     maxPasswordRequest: process.env.PASSWORD_MAX_REQUEST_LIMIT || 10,
   }),
