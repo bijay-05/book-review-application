5c61d3be9c3c4ddd83d05e2e6045a307ef83043d
bijay-05 <hawkeyesniper69@gmail.com>
2026-09-02 14:39:26 +0545

    add key-value module to store key-value data in redis

---
+++ backend/src/common/key-value/key-value.module.ts
@@ -0,0 +1,45 @@
+import {
+  DynamicModule,
+  ForwardReference,
+  Global,
+  Module,
+  Provider,
+  Type,
+} from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
+import { RedisModule } from "../redis/redis.module";
+import { KeyValueRedisService } from "./services/key-value.redis.service";
+
+@Global()
+@Module({})
+export class KeyValueModule {
+  static forRoot({
+    useRedis,
+  }: {
+    useCache?: boolean;
+    useRedis?: boolean;
+  }): DynamicModule {
+    const providers: Provider<any>[] = [];
+    const imports: (
+      | DynamicModule
+      | Type<any>
+      | Promise<DynamicModule>
+      | ForwardReference<any>
+    )[] = [];
+
+    imports.push(ConfigModule);
+
+    if (useRedis) {
+      imports.push(RedisModule);
+      providers.push(KeyValueRedisService);
+    }
+
+    return {
+      module: KeyValueModule,
+      providers,
+      exports: providers,
+      controllers: [],
+      imports: [...imports],
+    };
+  }
+}
